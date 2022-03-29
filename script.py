6b0ab8d63fa7016f7a8abade0e77539f155a4d95
Maximilinano Osorio <mosorio@isi.edu>
2022-03-29 10:44:25 -0300

    fix: add dockerfile

--- script.py
+++ script.py
@@ -3,8 +3,23 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.stats.anova import anova_lm
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--inputs", nargs='*', help="Input file", required=True)
+parser.add_argument("--output", help="Output file", required=True)
+args = parser.parse_args()
+
+def merge_multiple_dataframe(inputs):
+    df = pd.DataFrame()
+    for input in inputs:
+        df = df.append(pd.read_csv(input))
+    return df
+
+
+
 # Import data
-bikes = pd.read_csv('bikes.csv')
+bikes = merge_multiple_dataframe(args.inputs)
 
 # Fit model1
 model1 = sm.OLS.from_formula('cnt ~ temp + hum + windspeed', data=bikes).fit()
