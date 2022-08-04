fa86fa04cb18e5639e8f37befed8a3fe11f04dce
Maximilinano Osorio <mosorio@isi.edu>
2022-08-04 15:42:20 -0400

    fix: add test

--- test_script.py
+++ test_script.py
@@ -2,7 +2,8 @@ from cProfile import run
 from itertools import combinations, permutations
 from script import run_model
 
-variables = ["Temperature", "Ambient Temperature", "Humidity", "Wind Speed", "Season", "Month", "Holiday", "Weekday", "Workday", "Weather Situation"]
-perm = combinations(variables, 6) 
-for p in perm:
-    run_model(["data/bikes-2019-2020-ny.csv"], p, "test.txt", None)
+def test_file1_method1():
+    variables = ["Temperature", "Ambient Temperature", "Humidity", "Wind Speed", "Season", "Month", "Holiday", "Weekday", "Workday", "Weather Situation"]
+    perm = combinations(variables, 6) 
+    for p in perm:
+        run_model(["data/bikes-2019-2020-ny.csv"], p, None, None)
